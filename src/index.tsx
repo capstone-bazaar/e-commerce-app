976c1243d292e0b0cecf51c30a41a18f2aa3f774
Ertan Özdemir <ertan_kubi@hotmail.com>
2023-03-03 21:57:41 +0300

    Add base64 converter for file uploading

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,8 @@ const httpLink = createHttpLink({
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('token');
+  const token =
+    localStorage.getItem('token') || sessionStorage.getItem('token');
 
   return {
     headers: {
