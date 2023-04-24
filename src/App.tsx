15faf920f012808d88a62afd55a3cf40bb7f4d16
akayaaa <alpay0880@gmail.com>
2023-04-24 16:47:05 +0300

    Edit Page created.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NotFoundPage from './Pages/NotFoundPage';
 import ProductPage from './Pages/Product';
 import SignUp from './Pages/Signup';
 import VerifyEmailPage from './Pages/VerifyEmailPage';
+import EditPage from './Pages/Edit';
 import { PrivateRoute } from './routes/PrivateRoute';
 import { ShoppingCartPage } from './Pages/ShoppingCart';
 
@@ -19,6 +20,7 @@ function App() {
           <Route element={<SignUp />} path="/signup"></Route>
           <Route element={<VerifyEmailPage />} path="/verification" />
           <Route element={<NotFoundPage />} path="*" />
+          <Route element={<EditPage />} path="/edit"></Route>
           <Route element={<PrivateRoute />}>
             <Route element={<ProductPage />} path="/products/:id"></Route>
           </Route>
